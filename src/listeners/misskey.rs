c5f9caea3574b88929a18d4a69de92f283b0fe28
Chris Pham <carbon@opulenthovel.com>
2023-07-07 15:26:59 -0700

    Formatted misskey.rs and made it so it only sends message if it has image + logic

--- src/listeners/misskey.rs
+++ src/listeners/misskey.rs
@@ -8,10 +8,25 @@ use super::Handler;
 #[derive(Deserialize)]
 struct Note {
     files: Vec<File>,
+    user: User,
+    text: String,
+    #[serde(rename(deserialize = "createdAt"))]
+    created_at: String,
 }
+
+#[derive(Deserialize, Clone)]
+struct User {
+    name: String,
+    username: String,
+    #[serde(rename(deserialize = "avatarUrl"))]
+    avatar_url: String,
+}
+
 #[derive(Deserialize, Clone)]
 struct File {
     url: String,
+    #[serde(rename(deserialize = "type"))]
+    file_type: String,
 }
 
 impl Handler {
@@ -30,20 +45,93 @@ impl Handler {
                     {
                         Ok(res) => match &res.json::<Note>().await {
                             Ok(parsed) => {
-                                for file in &parsed.files {
-                                    msg.reply(&ctx.http, file.url.clone())
-                                        .await
-                                        .unwrap();
+                                // Build iter of videos and images
+                                let mut videos = parsed
+                                    .files
+                                    .iter()
+                                    .filter(|f| f.file_type.contains("video"));
+                                let mut images = parsed
+                                    .files
+                                    .iter()
+                                    .filter(|f| f.file_type.contains("image"));
+
+                                // Build peekables and check if contains any value
+                                let contains_video = videos.by_ref().peekable().peek().is_some();
+                                let contains_image = images.by_ref().peekable().peek().is_some();
+
+                                // Shared res to prevent duplication
+                                let mut _res;
+
+                                // If there is a value in videos list, assume it's all videos
+                                if contains_video {
+                                    for file in videos {
+                                        // Just reply to poster with links to videos
+                                        _res = &msg.reply(&ctx.http, file.url.clone()).await;
+                                    }
+
+                                // If there's a value in image list, assume it's all images
+                                } else if contains_image {
+                                    // Build a message object to send to channel
+                                    let _res = msg
+                                        .channel_id
+                                        .send_message(&ctx.http, |m| {
+                                            // construct new iter for images because of embed format
+                                            m.add_embed(|e| {
+                                                e.author(|a| {
+                                                    a.icon_url(&parsed.user.avatar_url)
+                                                        .name(&parsed.user.name)
+                                                        .url(format!(
+                                                            "https://misskey.io/@{}",
+                                                            parsed.user.username
+                                                        ))
+                                                })
+                                                .description(&parsed.text)
+                                                .timestamp(parsed.created_at.as_str());
+
+                                                // Error handling on next value
+                                                if let Some(image) = images.next() {
+                                                    e.image(image.url.clone());
+                                                }
+
+                                                // Assign URL because discord groups via url
+                                                e.url(format!(
+                                                    "https://misskey.io/notes/{}",
+                                                    note_id.as_str()
+                                                ));
+
+                                                e
+                                            });
+
+                                            // For any leftover images, append more embeds with same url as above
+                                            for file in images {
+                                                m.add_embed(|e| {
+                                                    e.image(file.url.clone()).url(format!(
+                                                        "https://misskey.io/notes/{}",
+                                                        note_id.as_str()
+                                                    ))
+                                                });
+                                            }
+                                            // Append reply to message
+                                            m.reference_message(msg);
+                                            m
+                                        })
+                                        .await;
                                 }
                             }
-                            Err(_) => {}
+                            Err(_) => {
+                                println!("Error trying to read response")
+                            }
                         },
                         Err(_) => println!("Error trying to access api"),
                     }
                 }
-                None => {}
+                None => {
+                    // Didn't find the group somehow?, might not be a note or something
+                }
             },
-            None => {}
+            None => {
+                // Didn't find a regex max
+            }
         }
     }
 }
